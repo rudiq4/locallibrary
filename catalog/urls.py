aa17de7e879bfcc88224dfc40e631e9ae1ffe927
rudiq4 <rudikvovan@gmail.com>
2018-12-12 21:13:15 +0200

    process

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     url('', views.index, name='index'),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
+    # url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
+from django.http import HttpResponse, Http404
 
 
 def index(request):
@@ -20,3 +21,25 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+
+
+#class BookListView(generic.ListView):
+#    model = Book
+#    context_object_name = 'my_book_list'   # ваше собственное имя переменной контекста в шаблоне
+#    queryset = Book.objects.filter(title__icontains='war')[:5] # Получение 5 книг, содержащих слово 'war' в заголовке
+#    template_name = 'books/my_arbitrary_template_name_list.html'  # Определение имени вашего шаблона и его расположения
+
+
+# def book_detail_view(request, pk):
+#     try:
+#         book_id = Book.objects.get(pk=pk)
+#     except Book.DoesNotExist:
+#         raise Http404("Book does not exist")
+#
+#     # book_id=get_object_or_404(Book, pk=pk)
+#
+#     return render(
+#         request,
+#         'catalog/book_detail.html',
+#         context={'book': book_id, }
+#     )
